57c9d54ad8bea07d741d4dc72a861c5acdf07b2e
Luiz Felipe Fonseca Rosa <luizfelipefonsecarosa@gmail.com>
2024-05-06 17:04:00 -0300

    cummiiiiiiiiiiiiing

--- file.h
+++ file.h
@@ -8,9 +8,6 @@ typedef struct __attribute__((__packed__)) {
     char name[256];
 } DirEntry;
 
-int delete_entry (Image image, char entry_name[256]) {
-
-}
 
 void export_file(Image image, DirEntry entry, FILE *out_file) {
     if (entry.file_size == 0) {
@@ -96,9 +93,63 @@ int import_file (Image image, FILE* new_file, char file_name[256]) {
     return 0;
 }
 
-DirEntry find_by_name (Image image, char name[256], int* err) {
+DirEntry find_by_name (Image image, char name[256], int* err, Pointer* pointer,  int* offset) {
     DirEntry entry;
     fseek(image.file, image.meta.block_size, SEEK_SET);
-    while(fread(&entry, sizeof(DirEntry), 1, image.file) && strcmp(entry.name, name));
+    BlockIter it = block_iter(image, 1);
+    void *buf = next_block(&it, NULL);
+    for(DirEntry *cur_dir = buf; cur_dir != NULL; cur_dir = next_block(&it, cur_dir)){
+        //CUMMING
+        pointer = 0;
+        for (DirEntry *cur_entry = cur_dir; cur_entry - cur_dir < image.meta.block_size / sizeof(DirEntry); cur_entry++)) {
+            if(cur_entry->type != 'D' && !strcmp(cur_entry->name, name)) {
+                entry = *cur_entry;
+                break;
+            }
+        }
+        pointer++;
+    }
+    free(it.pointer_block);
+    free(buf);
     return entry;
 }
+
+int clean_block(Image image, Pointer p) {
+    fseek(image.file, p, SEEK_SET);
+    for(int c = 0; c < image.meta.block_size; c++) {
+        fputc(0, image.file);
+    }
+}
+
+void update_entry (Image image, DirEntry entry, int offset) {
+    fseek(image.file, image.meta.block_size + offset * sizeof(DirEntry), SEEK_SET);
+}
+
+int delete_entry (Image image, char entry_name[256]) {
+    int find_errors = 0;
+    int offset = 0;
+    DirEntry entry = find_by_name(image, entry_name, &find_errors, &offset);
+    if (find_errors > 0) {
+        fprintf(stderr, "%d errors findind the name of file\n", find_errors);
+    }
+    Pointer main_pointer = entry.pointer;
+
+    fseek(image.file, main_pointer, SEEK_SET);
+    int block_read = 0;
+    Pointer p;
+
+    while (block_read < sizeof(Pointer)) {
+        fread(&p, sizeof (Pointer), 1, image.file);
+        free_block(image, p);
+        block_read+=sizeof (Pointer);
+    }
+
+    fseek(image.file, main_pointer, SEEK_SET);
+    free_block(image, main_pointer);
+    clean_block(image, main_pointer);
+
+    entry.type = 'D';
+    update_entry(image, entry, offset);
+}
+
+
